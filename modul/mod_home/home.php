60b4466e8852e9e68439f1c3d47c896bc47473c4
Nurul Yakin <nurulyakin01916@gmail.com>
2017-02-10 15:13:39 +0700

    test
    test

--- modul/mod_home/home.php
+++ modul/mod_home/home.php
@@ -99,15 +99,15 @@
     <div class="intro-marketing">
         <div class="container">
             <div class="row">
-<!--                 <div class="col-lg-4 col-md-4 intro-marketing-box">
-                    <h2><i class="fa fa-users"></i> Lihat Pendaftar</h2>
-                    <p>Daftar Peserta yang telah mendaftar untuk mengikuti seleksi Peserta Didik Baru.<a href="#"> Lihat Pendaftar</a></p>
-                </div> -->
-		        <div class="col-lg-6 col-md-6 intro-marketing-box">
+                <div class="col-lg-4 col-md-4 intro-marketing-box">
+                    <h2><i class="fa fa-users"></i> Prosedur Pendaftaran</h2>
+                    <p>Tata cara pendaftaran sekolah mitra bintaro.<a href="#" data-toggle="modal" data-target="#myModal"> Lihat Prosedur Pendaftaran</a></p>
+                </div>
+		        <div class="col-lg-4 col-md-4 intro-marketing-box">
                     <h2><i class="fa fa-envelope"></i> Bantuan Pendaftaran</h2>
                     <p>Tanya jawab Online seputar pendaftaran dengan panitia Penerimaan Peserta Didik Baru.<a href="?module=pesan"> Kirim Pesan</a></p>
                 </div>
-                <div class="col-lg-6 col-md-6 intro-marketing-box">
+                <div class="col-lg-4 col-md-4 intro-marketing-box">
                     <h2><i class="fa fa-user"></i> Sudah Punya Akun</h2>
                     <p>Setelah melakukan pendaftaran dan memiliki akun, kamu dapat mengatur data pribadi.<a href="media.php?module=login"> Login</a></p>
                 </div>
@@ -115,6 +115,26 @@
         </div>
     </div>
 
+    <!-- Modal -->
+    <div id="myModal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Prosedur Pendaftaran</h4>
+          </div>
+          <div class="modal-body">
+            <div style="background-image:url('http://psb-smamitrabintaro.web.id/images/prosedur.jpg');"></div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
     <!-- Page content -->
     <div class="templates">
         <div class="container">
