0d744318684a7fba0fc10aaad30de2e26d7c5721
Nurul Yakin <nurulyakin01916@gmail.com>
2017-02-11 13:00:45 +0700

    konfir
    konfir

--- modul/mod_pay/detail_pembayaran.php
+++ modul/mod_pay/detail_pembayaran.php
@@ -4,26 +4,8 @@ include "../../config/koneksi.php";
 $sql1 = mysql_query("SELECT * FROM psb_konfirmasi AS a JOIN psb_formulir AS b ON a.no_perserta=b.no_peserta WHERE b.no_peserta = '$_SESSION[no_peserta]' AND a.jenis_pembayarn = 'Formulir Pendaftaran'")or die(mysql_error());
 $row1=mysql_fetch_array($sql1);
 
-$sql2 = mysql_query("SELECT * FROM psb_konfirmasi AS a JOIN psb_formulir AS b ON a.no_perserta=b.no_peserta WHERE b.no_peserta = '$_SESSION[no_peserta]' AND a.jenis_pembayarn = 'MOS'")or die(mysql_error());
+$sql2 = mysql_query("SELECT * FROM psb_konfirmasi AS a JOIN psb_formulir AS b ON a.no_perserta=b.no_peserta WHERE b.no_peserta = '$_SESSION[no_peserta]' AND a.jenis_pembayarn = 'Biaya Lainnya'")or die(mysql_error());
 $row2=mysql_fetch_array($sql2);
-
-$sql3 = mysql_query("SELECT * FROM psb_konfirmasi AS a JOIN psb_formulir AS b ON a.no_perserta=b.no_peserta WHERE b.no_peserta = '$_SESSION[no_peserta]' AND a.jenis_pembayarn = 'SPP'")or die(mysql_error());
-$row3=mysql_fetch_array($sql3);
-
-$sql4 = mysql_query("SELECT * FROM psb_konfirmasi AS a JOIN psb_formulir AS b ON a.no_perserta=b.no_peserta WHERE b.no_peserta = '$_SESSION[no_peserta]' AND a.jenis_pembayarn = 'Seragam'")or die(mysql_error());
-$row4=mysql_fetch_array($sql4);
-
-$sql5 = mysql_query("SELECT * FROM psb_konfirmasi AS a JOIN psb_formulir AS b ON a.no_perserta=b.no_peserta WHERE b.no_peserta = '$_SESSION[no_peserta]' AND a.jenis_pembayarn = 'LKS Semester 1'")or die(mysql_error());
-$row5=mysql_fetch_array($sql5);
-
-$sql6 = mysql_query("SELECT * FROM psb_konfirmasi AS a JOIN psb_formulir AS b ON a.no_perserta=b.no_peserta WHERE b.no_peserta = '$_SESSION[no_peserta]' AND a.jenis_pembayarn = 'Atribut'")or die(mysql_error());
-$row6=mysql_fetch_array($sql6);
-
-$sql7 = mysql_query("SELECT * FROM psb_konfirmasi AS a JOIN psb_formulir AS b ON a.no_perserta=b.no_peserta WHERE b.no_peserta = '$_SESSION[no_peserta]' AND a.jenis_pembayarn = 'Cover Ijazah'")or die(mysql_error());
-$row7=mysql_fetch_array($sql7);
-
-$sql8 = mysql_query("SELECT * FROM psb_konfirmasi AS a JOIN psb_formulir AS b ON a.no_perserta=b.no_peserta WHERE b.no_peserta = '$_SESSION[no_peserta]' AND a.jenis_pembayarn = 'Kartu Pelajar & Photo'")or die(mysql_error());
-$row8=mysql_fetch_array($sql8);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -67,7 +49,7 @@ $row8=mysql_fetch_array($sql8);
     <tr>
     <td>MOS</td>
     <td>:</td>
-    <td colspan="4"><input name="mos" type="text" id="mos" readonly="true" value="<?= $row2['jenis_pembayarn']=='MOS' && $row2['total_pembayaran']>=150000 ? 'LUNAS' : 'BELUM LUNAS' ?>" />&nbsp;*Rp. 150.000,-</td>
+    <td colspan="4"><input name="mos" type="text" id="mos" readonly="true" value="<?= $row2['jenis_pembayarn']=='Biaya Lainnya' && $row2['total_pembayaran']>=150000 ? 'LUNAS' : 'BELUM LUNAS' ?>" />&nbsp;*Rp. 150.000,-</td>
     </tr>
     <tr>
       <td>
@@ -77,7 +59,7 @@ $row8=mysql_fetch_array($sql8);
     <tr>
     <td>SPP</td>
     <td>:</td>
-    <td colspan="4"><input name="spp" type="text" id="spp" readonly="true" value="<?= $row3['jenis_pembayarn']=='SPP' && $row3['total_pembayaran']>=135000 ? 'LUNAS' : 'BELUM LUNAS' ?>" />&nbsp;*Rp. 135.000,-</td>
+    <td colspan="4"><input name="spp" type="text" id="spp" readonly="true" value="<?= $row2['jenis_pembayarn']=='Biaya Lainnya' && $row2['total_pembayaran']>=135000 ? 'LUNAS' : 'BELUM LUNAS' ?>" />&nbsp;*Rp. 135.000,-</td>
     </tr>
     <tr>
       <td>
@@ -87,7 +69,7 @@ $row8=mysql_fetch_array($sql8);
     <tr>
     <td>Seragam</td>
     <td>:</td>
-    <td colspan="4"><input name="srm" type="text" id="srm" readonly="true" value="<?= $row4['jenis_pembayarn']=='Seragam' && $row4['total_pembayaran']>=350000 ? 'LUNAS' : 'BELUM LUNAS' ?>" />&nbsp;*Rp. 350.000,-</td>
+    <td colspan="4"><input name="srm" type="text" id="srm" readonly="true" value="<?= $row2['jenis_pembayarn']=='Biaya Lainnya' && $row2['total_pembayaran']>=350000 ? 'LUNAS' : 'BELUM LUNAS' ?>" />&nbsp;*Rp. 350.000,-</td>
     </tr>
     <tr>
       <td>
@@ -97,7 +79,7 @@ $row8=mysql_fetch_array($sql8);
     <tr>
     <td>LKS Semester 1</td>
     <td>:</td>
-    <td colspan="4"><input name="lks" type="text" id="lks" readonly="true" value="<?= $row5['jenis_pembayarn']=='LKS Semester 1' && $row5['total_pembayaran']>=150000 ? 'LUNAS' : 'BELUM LUNAS' ?>" />&nbsp;*Rp. 150.000,-</td>
+    <td colspan="4"><input name="lks" type="text" id="lks" readonly="true" value="<?= $row2['jenis_pembayarn']=='Biaya Lainnya' && $row2['total_pembayaran']>=150000 ? 'LUNAS' : 'BELUM LUNAS' ?>" />&nbsp;*Rp. 150.000,-</td>
     </tr>
     <tr>
       <td>
@@ -107,7 +89,7 @@ $row8=mysql_fetch_array($sql8);
     <tr>
     <td>Atribut</td>
     <td>:</td>
-    <td colspan="4"><input name="atrib" type="text" id="atrib" readonly="true" value="<?= $row6['jenis_pembayarn']=='Atribut' && $row6['total_pembayaran']>=70000 ? 'LUNAS' : 'BELUM LUNAS' ?>" />&nbsp;*Rp. 70.000,-</td>
+    <td colspan="4"><input name="atrib" type="text" id="atrib" readonly="true" value="<?= $row2['jenis_pembayarn']=='Biaya Lainnya' && $row2['total_pembayaran']>=70000 ? 'LUNAS' : 'BELUM LUNAS' ?>" />&nbsp;*Rp. 70.000,-</td>
     </tr>
     <tr>
       <td>
@@ -117,7 +99,7 @@ $row8=mysql_fetch_array($sql8);
     <tr>
     <td>Cover Ijazah</td>
     <td>:</td>
-    <td colspan="4"><input name="cover" type="text" id="cover" readonly="true" value="<?= $row7['jenis_pembayarn']=='Cover Ijazah' && $row7['total_pembayaran']>=100000 ? 'LUNAS' : 'BELUM LUNAS' ?>" />&nbsp;*Rp. 100.000,-</td>
+    <td colspan="4"><input name="cover" type="text" id="cover" readonly="true" value="<?= $row2['jenis_pembayarn']=='Biaya Lainnya' && $row2['total_pembayaran']>=100000 ? 'LUNAS' : 'BELUM LUNAS' ?>" />&nbsp;*Rp. 100.000,-</td>
     </tr>
     <tr>
       <td>
@@ -127,7 +109,7 @@ $row8=mysql_fetch_array($sql8);
     <tr>
     <td>Kartu Pelajar & Photo</td>
     <td>:</td>
-    <td colspan="4"><input name="kts" type="text" id="kts" readonly="true" value="<?= $row8['jenis_pembayarn']=='Kartu Pelajar & Photo' && $row8['total_pembayaran']>=70000 ? 'LUNAS' : 'BELUM LUNAS' ?>" />&nbsp;*Rp. 70.000,-</td>
+    <td colspan="4"><input name="kts" type="text" id="kts" readonly="true" value="<?= $row2['jenis_pembayarn']=='Biaya Lainnya' && $row2['total_pembayaran']>=70000 ? 'LUNAS' : 'BELUM LUNAS' ?>" />&nbsp;*Rp. 70.000,-</td>
     <td><p class="center"><a target="_blank" href="modul/mod_pay/f_pay.php?id=<?= $_SESSION[no_peserta] ?>" class="btn btn-primary fa fa-1x fa-border fa-print"></a></p></td>
     <td><p>* Yang harus dibayar</p>
     <p>** Yang dibayar</p></td>
